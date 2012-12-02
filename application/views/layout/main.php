77ff08c66d31cb84c75b7068e62e2c92ad2b4f20
chokkoyamada <n.yamada@cyberagentamerica.com>
2012-12-02 07:31:38 +0000

    added layout for codeigniter

---
+++ application/views/layout/main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja">
+    <head>
+        <title><?php echo $title; ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+        <script src="http://code.jquery.com/jquery-latest.js"></script>
+        <link href="/css/bootstrap.css" rel="stylesheet" media="screen">
+        <link href="/css/bootstrap-responsive.css" rel="stylesheet" media="screen">
+        <script src="/js/bootstrap.js"></script>
+    </head>
+    <body>
+        <?php echo $content_for_layout; ?>
+    </body>
+</html>
