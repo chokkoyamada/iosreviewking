af49e9863b82c4da2a3924ff109dbbfc984b3cbe
chokkoyamada <n.yamada@cyberagentamerica.com>
2012-12-02 16:34:00 +0000

    added featured apps

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -12,4 +12,12 @@
         <h4>Today's Featured</h4>
     </div>
   </div>
+  <div class='row'>
+    <div class='span4'>
+        <a href='/index/app_detail/282614216'><img src='http://a1243.phobos.apple.com/us/r1000/076/Purple/v4/89/15/98/8915982d-6fe0-26f6-fcd4-64d67adcd698/icon_57x57.png' /></a>
+        <a href='/index/app_detail/283646709'><img src='http://a683.phobos.apple.com/us/r1000/119/Purple/v4/c8/60/73/c86073c6-655c-ad50-eb8f-771cc1e9bffa/desktop_icon.png' /></a>
+        <a href='/index/app_detail/284035177'><img src='http://a1349.phobos.apple.com/us/r1000/087/Purple/v4/d8/27/bc/d827bce1-ccbc-29b1-4f15-0c66c558a77a/Icon.png' /></a>
+        <a href='/index/app_detail/284882215'><img src='http://a446.phobos.apple.com/us/r1000/100/Purple/v4/5d/c7/86/5dc7867d-4c41-1e76-90ca-f20327144dd8/Icon.png' /></a>
+    </div>
+  </div>
 </div>
